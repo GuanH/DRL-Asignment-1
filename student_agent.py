6bbf1f011a745b4c2b99c792fcef09b008b2f718
GuanH <r13922167@ntu.edu.tw>
2025-03-14 00:05:18 +0800

    init

--- student_agent.py
+++ student_agent.py
@@ -1,18 +1,45 @@
-# Remember to adjust your student ID in meta.xml
 import numpy as np
-import pickle
-import random
-import gym
+
+q_table = np.fromfile('q_table.npz').reshape((19, 19, 2, 2, 2, 2, 2, 2, 2, 6))
+
+check = 0
+stage = 0
+drop = (-1, -1)
+
 
 def get_action(obs):
-    
-    # TODO: Train your own agent
-    # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
-    # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
-    #       Otherwise, even if your agent performs well in training, it may fail during testing.
+    global check, stage, drop
 
+    stations = [(obs[2+i*2], obs[2+i*2+1]) for i in range(4)]
+    x, y = obs[0], obs[1]
+    if drop[0] == -1:
+        tx, ty = stations[check]
+    else:
+        tx, ty = drop
 
-    return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
-    # You can submit this random agent to evaluate the performance of a purely random strategy.
+    s = [tx-x+9, ty-y+9, stage, obs[10], obs[11],
+         obs[12], obs[13], obs[14], obs[15]]
 
+    if np.random.rand() < 0.1:
+        action = np.random.randint(0, 4)
+    else:
+        action = np.argmax(
+            q_table[s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]])
+    if stage == 0:
+        if (x, y) == (tx, ty):
+            if obs[14] == 1:
+                if action == 4:
+                    stage = 1
+                    check = 0
+                    drop = (-1, -1)
+            else:
+                check = (check+1) % 4
+    else:
+        if (x, y) == stations[check]:
+            if obs[15] != 1:
+                check = (check+1) % 4
+        if action == 5:
+            drop = (obs[0], obs[1])
+            stage = 0
+
+    return action

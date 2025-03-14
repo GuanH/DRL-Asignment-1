cda602cb973f8aa3be60e76650ffba9e0e681e4a
GuanH <r13922167@ntu.edu.tw>
2025-03-14 12:28:28 +0800

    again

--- student_agent.py
+++ student_agent.py
@@ -1,10 +1,14 @@
 import numpy as np
 
-q_table = np.fromfile('q_table.npz').reshape((19, 19, 2, 2, 2, 2, 2, 2, 2, 6))
+q_table = np.fromfile('q_table.npz').reshape((3, 3, 2, 2, 2, 2, 2, 2, 2, 6))
 
 check = 0
 stage = 0
 drop = (-1, -1)
+
+
+def sign(x):
+    return 1 if x > 0 else 2 if x < 0 else 0
 
 
 def get_action(obs):
@@ -17,14 +21,26 @@ def get_action(obs):
     else:
         tx, ty = drop
 
-    s = [tx-x+9, ty-y+9, stage, obs[10], obs[11],
+    s = [sign(tx-x), sign(ty-y), stage, obs[10], obs[11],
          obs[12], obs[13], obs[14], obs[15]]
 
     if np.random.rand() < 0.1:
-        action = np.random.randint(0, 4)
+        v = np.random.rand(6)
+        v[4] = 0
+        v[5] = 0
     else:
-        action = np.argmax(
-            q_table[s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]])
+        v = q_table[s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]]
+    # 0 down 1 up 2 right 3 left
+    if obs[10]:  # north
+        v[1] = 0
+    if obs[11]:  # south
+        v[0] = 0
+    if obs[12]:  # east
+        v[2] = 0
+    if obs[13]:  # west
+        v[3] = 0
+    action = np.argmax(v)
+
     if stage == 0:
         if (x, y) == (tx, ty):
             if obs[14] == 1:
--- train.py
+++ train.py
@@ -5,7 +5,7 @@ I trained multiple runs with different epoch, eps, eps_rate1 and eps_rate2
 and I didn't remember the actual recipe.
 """
 
-EPOCH = 400000
+EPOCH = 300000
 FUEL = 200
 alpha = 0.1
 gamma = 0.99
@@ -18,14 +18,18 @@ def get_action_name(action):
     return actions[action] if action is not None else "None"
 
 
-# q_table = np.zeros((19, 19, 2, 2, 2, 2, 2, 2, 2, 6))
-q_table = np.fromfile('q_table.npz').reshape((19, 19, 2, 2, 2, 2, 2, 2, 2, 6))
-eps = 0.2
+q_table = np.ones((3, 3, 2, 2, 2, 2, 2, 2, 2, 6)) * -100
+# q_table = np.fromfile('q_table.npz').reshape((3, 3, 2, 2, 2, 2, 2, 2, 2, 6))
+eps = 0.7
 
-eps_rate1 = (0.1/0.2)**(1/(EPOCH * 0.7))
-eps_rate2 = (0.01/0.1)**(1/(EPOCH * 0.3))
+eps_rate1 = (0.2/0.7)**(1/(EPOCH * 0.3))
+eps_rate2 = (0.01/0.2)**(1/(EPOCH * 0.7))
 taxi = MultiTaxiWrapper(1, 10, FUEL, 15)
 reward_hist = []
+
+
+def sign(x):
+    return 1 if x > 0 else 2 if x < 0 else 0
 
 
 for epoch in range(EPOCH):
@@ -39,7 +43,7 @@ for epoch in range(EPOCH):
     stations = [(obs[2+i*2], obs[2+i*2+1]) for i in range(4)]
     tx, ty = stations[check]
 
-    s = [tx-x+9, ty-y+9, stage, obs[10], obs[11],
+    s = [sign(tx-x), sign(ty-y), stage, obs[10], obs[11],
          obs[12], obs[13], obs[14], obs[15]]
     # taxi.render(0)
     for step in range(FUEL):
@@ -52,15 +56,23 @@ for epoch in range(EPOCH):
 
         if np.random.rand() < eps:
             r = np.random.randint(0, 10)
+            v = np.random.rand(6)
             if r == 0:
-                action = 1 if s[0]-5 < 0 else 0
+                v[1 if tx-x < 0 else 0] = 2
             elif r == 1:
-                action = 3 if s[1]-5 < 0 else 2
-            else:
-                action = np.random.randint(0, 6)
+                v[3 if ty-y < 0 else 2] = 2
         else:
-            action = np.argmax(
-                q_table[s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]])
+            v = q_table[s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8]]
+        # 0 down 1 up 2 right 3 left
+        if obs[10]:  # north
+            v[1] = 0
+        if obs[11]:  # south
+            v[0] = 0
+        if obs[12]:  # east
+            v[2] = 0
+        if obs[13]:  # west
+            v[3] = 0
+        action = np.argmax(v)
         nobs, reward, done = taxi.step(np.array([action]))
         nobs = nobs[0]
         ncheck = check
@@ -72,7 +84,7 @@ for epoch in range(EPOCH):
                     if action == 4:
                         nstage = 1
                         ncheck = 0
-                        reward += 5
+                        reward += 10
                         drop = (-1, -1)
                     else:
                         reward -= 1
@@ -83,14 +95,14 @@ for epoch in range(EPOCH):
             if (x, y) == stations[check]:
                 if obs[15] == 1:
                     if action == 5:
-                        reward += 10
+                        reward += 50
                     else:
                         reward -= 1
                 else:
                     ncheck = (check+1) % 4
                     reward += 0.1
             if action == 5:
-                reward -= 7
+                reward -= 15
                 drop = (nobs[0], nobs[1])
                 nstage = 0
         # print(f'({obs[0]}, {obs[1]}) ({tx}, {ty})')
@@ -105,7 +117,7 @@ for epoch in range(EPOCH):
         else:
             ntx, nty = drop
 
-        ns = [ntx-nx+9, nty-ny+9, nstage,
+        ns = [sign(ntx-nx), sign(nty-ny), nstage,
               nobs[10], nobs[11], nobs[12], nobs[13], nobs[14], nobs[15]]
         tot_reward += reward
 
@@ -119,7 +131,7 @@ for epoch in range(EPOCH):
         s = ns
         if done:
             break
-    if eps > 0.1:
+    if eps > 0.2:
         eps *= eps_rate1
     else:
         eps *= eps_rate2
